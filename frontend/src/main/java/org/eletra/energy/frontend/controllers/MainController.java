0c40658f47e0e8c4c69b9d48ff2cc4bdee01d339
Hiel Saraiva <hielsaraiva11.hs@gmail.com>
2025-12-04 21:48:24 -0300

    refactor: improving project02 code

--- frontend/src/main/java/org/eletra/energy/frontend/controllers/MainController.java
+++ frontend/src/main/java/org/eletra/energy/frontend/controllers/MainController.java
@@ -11,90 +11,86 @@ import org.eletra.energy.backend.models.LineMeter;
 import org.eletra.energy.backend.models.ModelMeter;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainController {
-	// Componentes FXML
-	@FXML
-	private ComboBox<String> comboBox;
-	@FXML
-	private TreeView<String> treeView;
-	@FXML
-	private TreeItem<String> treeItem;
-	@FXML
-	private TitledPane titledPaneLinhas;
-	@FXML
-	private TitledPane titledPaneModelos;
+    // Componentes FXML
+    @FXML
+    private ComboBox<String> comboBox;
+    @FXML
+    private TreeView<String> treeView;
+    @FXML
+    private TreeItem<String> treeItem;
+    @FXML
+    private TitledPane titledPaneLinhas;
+    @FXML
+    private TitledPane titledPaneModelos;
 
     // Atributos auxiliares
     private BackController backController;
 
-	private void loadComboBox() {
-		ArrayList<String> meterLinesNames = new ArrayList<String>();
+    private void loadComboBox() {
+        ArrayList<String> meterLinesNames = new ArrayList<String>();
 
-		for (LineMeter meter : backController.getMeterLines()) {
-			meterLinesNames.add(meter.getName());
-		}
+        for (LineMeter meter : backController.getMeterLines()) {
+            meterLinesNames.add(meter.getName());
+        }
 
-		comboBox.getItems().addAll(meterLinesNames);
-	}
+        comboBox.getItems().addAll(meterLinesNames);
+    }
 
-	private void loadTreeItem(String loadLine) {
+    private void loadTreeItem(String loadLine) {
 
-		int j = 0;
+        LineMeter selectedLine = backController.getMeterLines().stream()
+                .filter(line -> loadLine.equals(line.getName()))
+                .findFirst()
+                .orElse(null);
 
-		for (LineMeter line : backController.getMeterLines()) {
-			if (loadLine.equals(line.getName())) {
-				break;
-			}
-			++j;
-		}
+        List<TreeItem<String>> categories = selectedLine.getMeterCategories().stream()
+                .map(cat -> new TreeItem<>(cat.getName()))
+                .collect(Collectors.toList());
 
-		ArrayList<TreeItem<String>> categories = new ArrayList<>();
+        treeItem.getChildren().setAll(categories);
 
-		for (CategoryMeter cat : backController.getMeterLines().get(j).getMeterCategories()) {
-			categories.add(new TreeItem<>(cat.getName()));
-		}
+        for (int i = 0; i < selectedLine.getMeterCategories().size(); i++) {
 
-		treeItem.getChildren().setAll(categories);
+            CategoryMeter category = selectedLine.getMeterCategories().get(i);
 
-		for (int i = 0; i < backController.getMeterLines().get(j).getMeterCategories().size(); i++) {
+            List<TreeItem<String>> modelItems = category.getMeterModels().stream()
+                    .map(mod -> new TreeItem<>(mod.getName()))
+                    .collect(Collectors.toList());
 
-			CategoryMeter category = backController.getMeterLines().get(j).getMeterCategories().get(i);
+            TreeItem<String> categoryTreeItem = treeItem.getChildren().get(i);
 
-			ArrayList<TreeItem<String>> models = new ArrayList<>();
+            categoryTreeItem.getChildren().setAll(modelItems);
+            categoryTreeItem.setExpanded(true);
+        }
+    }
 
-			for (ModelMeter mod : category.getMeterModels()) {
-				models.add(new TreeItem<>(mod.getName()));
-			}
-
-			treeItem.getChildren().get(i).getChildren().setAll(models);
-			treeItem.getChildren().get(i).setExpanded(true);
-		}
-	}
-
-	// Métodos FXML
-	@FXML
-	public void initialize() {
+    // Métodos FXML
+    @FXML
+    public void initialize() {
         backController = new BackController();
-		backController.instantiateLines();
-		loadComboBox();
-	}
+        backController.instantiateLines();
+        loadComboBox();
+    }
 
-	@FXML
-	public void onLineSelect() {
-		try {
-			treeView.setShowRoot(false);
+    @FXML
+    public void onLineSelect() {
+        try {
+            treeView.setShowRoot(false);
 
-			String selectedLineName = comboBox.getValue();
+            String selectedLineName = comboBox.getValue();
 
-			loadTreeItem(selectedLineName);
+            loadTreeItem(selectedLineName);
 
-			treeItem.setExpanded(true);
-			titledPaneModelos.setDisable(false);
-			titledPaneModelos.setExpanded(true);
+            treeItem.setExpanded(true);
+            titledPaneModelos.setDisable(false);
+            titledPaneModelos.setExpanded(true);
 
-		} catch (Exception e) {
-			System.out.println("Error on line select: " + e.getMessage());
-		}
-	}
+        } catch (Exception e) {
+            System.out.println("Error on line select: " + e.getMessage());
+        }
+    }
 }
